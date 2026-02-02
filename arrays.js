1a0d638830d9676f8c35b468d08785ed80a6c915
TJ <tjshotta24@gmail.com>
2026-02-02 12:56:13 +0100

    opgave 2

--- arrays.js
+++ arrays.js
@@ -3,9 +3,13 @@ let dogsArray = ["Barney", "Molly", "Baxter", "Polly", "Buddy", "Suki", "Watson"
 let fruitsArray = ["Banana", "Orange", "Pineapple", "Mango", "Plum", "Apricot"]
 let numbersArray = [12, 4, 45, 17, 2, 26, 7, 55, 48, 32, 21]
 
+// opgave 1
 // --- Løs opgaverne herunder --- 
 let newNumbers = numbersArray.filter(num => num > 20)
 console.log("Problem 1", newNumbers)
 
 
+// opgave 2 
 
+catsArray.push("Sniffles")
+console.log("Opgave 2", catsArray)
